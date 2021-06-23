53aca9cd50a34f89e8b30afe8407aad7842821e9
cornelia chirila <cornelia.calarasiu@yahoo.it>
2021-06-24 00:35:11 +0300

    change the url for the breadcrumbs

--- public/javascripts/breadcrumbs.js
+++ public/javascripts/breadcrumbs.js
@@ -1,11 +1,12 @@
 exports.get_breadcrumbs = function(url) {
   let rtn = [{name: "HOME", url: "/"}],
       acc = "", // accumulative url
-      arr = url.substring(1).split("/");
-
+      modifiedURL=url.slice(url.substring(1),url.indexOf('?'))
+      arr = modifiedURL.substring(1).split("/");
+      console.log(modifiedURL);
   for (i=0; i<arr.length; i++) {
-      acc = i != arr.length-1 ? acc+"/"+arr[i] : null;
-      rtn[i+1] = {name: arr[i].toUpperCase(), url: acc};
+    acc = i != arr.length-1 ? acc+"/"+arr[i] : null;
+    rtn[i+1] = {name: arr[i].toUpperCase(), url: acc};
   }
   return rtn;
 };
