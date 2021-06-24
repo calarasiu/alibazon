e419591b4a0a74148f8e30cdbb7279636cd4c8a7
cornelia chirila <cornelia.calarasiu@yahoo.it>
2021-06-24 18:55:34 +0300

    changed the category_controller

--- public/javascripts/breadcrumbs.js
+++ public/javascripts/breadcrumbs.js
@@ -3,10 +3,6 @@ exports.get_breadcrumbs = function(url) {
       acc = "", // accumulative url
       modifiedURL=url.slice(url.substring(1),url.indexOf('?'))
       arr = modifiedURL.substring(1).split("/");
-<<<<<<< HEAD
-=======
-      console.log(modifiedURL);
->>>>>>> parent of 3990648... Revert "change the url for the breadcrumbs"
   for (i=0; i<arr.length; i++) {
     acc = i != arr.length-1 ? acc+"/"+arr[i] : null;
     rtn[i+1] = {name: arr[i].toUpperCase(), url: acc};
